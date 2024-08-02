f2cf181d69d3672cb9515ca89c0333aafe27b1e8
Suellen Moreira Lima <sulima19977991@gmail.com>
2024-08-02 14:40:22 -0300

    fix: remove comments end unused class

--- src/main/java/com/agency/bankibm/controller/LoginController.java
+++ src/main/java/com/agency/bankibm/controller/LoginController.java
@@ -2,10 +2,7 @@ package com.agency.bankibm.controller;
 
 import com.agency.bankibm.dto.LoginRequestDTO;
 import com.agency.bankibm.dto.LoginResponseDTO;
-import com.agency.bankibm.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
--- src/main/java/com/agency/bankibm/service/ClientService.java
+++ src/main/java/com/agency/bankibm/service/ClientService.java
@@ -1,13 +1,10 @@
-// ClientService.java
 package com.agency.bankibm.service;
 
 import com.agency.bankibm.dto.ClientDTO;
 import com.agency.bankibm.model.Account;
 import com.agency.bankibm.model.Client;
-import com.agency.bankibm.model.Login;
 import com.agency.bankibm.repository.AccountRepository;
 import com.agency.bankibm.repository.ClientRepository;
-import com.agency.bankibm.repository.LoginRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,10 +17,12 @@ import java.util.Optional;
 @Transactional
 public class ClientService {
 
+    // Declaração dos repositórios usados pelo serviço
     private final ClientRepository clientRepository;
     private final AccountRepository accountRepository;
     private final LoginRepository loginRepository;
 
+    // Construtor que inicializa os repositórios com injeção de dependência
     @Autowired
     public ClientService(ClientRepository clientRepository, AccountRepository accountRepository, LoginRepository loginRepository) {
         this.clientRepository = clientRepository;
@@ -31,72 +30,88 @@ public class ClientService {
         this.loginRepository = loginRepository;
     }
 
+    // Método que retorna todos os clientes como uma lista de ClientDTO
     public List<ClientDTO> getAll() {
-        List<Client> lista =  clientRepository.findAll();
+        // Busca todos os clientes no repositório
+        List<Client> lista = clientRepository.findAll();
 
+        // Cria uma lista de ClientDTO
         List<ClientDTO> listaDTO = new ArrayList<>();
+        // Converte cada Client para ClientDTO e adiciona à lista
         for (Client usuarioEntity : lista) {
-            listaDTO.add(usuarioEntity.toDTO() );
+            listaDTO.add(usuarioEntity.toDTO());
         }
 
+        // Retorna a lista de ClientDTO
         return listaDTO;
     }
 
+    // Método que retorna um cliente específico com base no ID
     public ClientDTO getOne(int idClient) {
-
+        // Busca o cliente pelo ID no repositório
         Optional<Client> optional = clientRepository.findById(idClient);
 
-        Client professor = optional.orElse( new Client() );
+        // Se o cliente não for encontrado, cria um novo objeto Client vazio
+        Client professor = optional.orElse(new Client());
 
+        // Converte o cliente para ClientDTO e retorna
         return professor.toDTO();
     }
 
+    // Método para salvar um novo cliente
     @Transactional
     public ClientDTO saveClient(ClientDTO clientDTO) {
-
+        // Cria uma nova conta e a salva no repositório
         Account account = new Account();
         accountRepository.save(account);
 
+        // Converte o ClientDTO para Client e associa a conta criada
         Client client = clientDTO.toEntity();
         client.setAccount(account); // Associa a conta ao cliente
         clientRepository.save(client);
 
+        // Se houver dados de login no ClientDTO, cria e salva um novo login
         if (clientDTO.getLogin() != null) {
-
             Login login = new Login();
             login.setEmail(clientDTO.getLogin().getEmail());
             login.setPassword(clientDTO.getLogin().getPassword());
             loginRepository.save(login);
         }
 
-
-        return client.toDTO(); // Retorna o DTO do cliente criado
+        // Retorna o DTO do cliente criado
+        return client.toDTO();
     }
 
-
+    // Método para atualizar um cliente existente
     public ClientDTO updateClient(int idClient, ClientDTO clientDTO) {
+        // Busca o cliente pelo ID no repositório
         Optional<Client> optional = clientRepository.findById(idClient);
 
-        if(optional.isPresent() == true ){
+        // Se o cliente for encontrado, atualiza seus dados
+        if (optional.isPresent()) {
             Client clientBd = optional.get();
             clientBd.setName(clientDTO.getName());
             clientBd.setAge(clientDTO.getAge());
             clientBd.setEmail(clientDTO.getEmail());
             clientBd.setNumberAccount(clientDTO.getNumberAccount());
 
-           if (clientBd.getAccount() != null && clientDTO.getAccount() != null) {
-               clientBd.getAccount().setBalance(clientDTO.getAccount().getBalance());
-               clientBd.getAccount().setTotalLimit(clientDTO.getAccount().getTotalLimit());
-           }
+            // Atualiza os dados da conta, se existirem
+            if (clientBd.getAccount() != null && clientDTO.getAccount() != null) {
+                clientBd.getAccount().setBalance(clientDTO.getAccount().getBalance());
+                clientBd.getAccount().setTotalLimit(clientDTO.getAccount().getTotalLimit());
+            }
 
+            // Salva o cliente atualizado e retorna seu DTO
             return clientRepository.save(clientBd).toDTO();
-        }
-        else {
+        } else {
+            // Se o cliente não for encontrado, retorna um DTO vazio
             return new Client().toDTO();
         }
     }
 
+    // Método para deletar um cliente pelo ID
     public void delete(int idClient) {
+        // Deleta o cliente pelo ID no repositório
         clientRepository.deleteById(idClient);
     }
 }
